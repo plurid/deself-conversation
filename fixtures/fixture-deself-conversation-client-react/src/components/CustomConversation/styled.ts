1d307256a4994b3498494e4792a5b348159639d4
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-06-18 12:19:34 +0300

    fixtures/fixture-deself-conversation-client-react > dev: App, CustomConversation, data

--- fixtures/fixture-deself-conversation-client-react/src/components/CustomConversation/styled.ts
+++ fixtures/fixture-deself-conversation-client-react/src/components/CustomConversation/styled.ts
@@ -4,11 +4,13 @@ import styled from 'styled-components';
 
 export const StyledCustomConversation = styled.div`
     padding: 1rem;
+    font-family: Ubuntu, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+        'Open Sans', 'Helvetica Neue', sans-serif;
 `;
 
 
 export const StyledMetadata = styled.div`
-    font-size: 0.7rem;
+    font-size: 0.8rem;
     margin-bottom: 1rem;
 
     display: flex;
@@ -21,4 +23,27 @@ export const StyledMetadataFrom = styled.div`
 
 
 export const StyledMetadataDate = styled.div`
+`;
+
+
+export const StyledMessage = styled.div`
+    font-size: 1rem;
+    line-height: 1.6;
+    padding: 1rem;
+`;
+
+
+export const StyledActions = styled.div`
+    margin-top: 1rem;
+    font-size: 0.8rem;
+    display: flex;
+`;
+
+
+export const StyledActionsReply = styled.div`
+    margin-right: 10px;
+`;
+
+
+export const StyledActionsReplies = styled.div`
 `;
