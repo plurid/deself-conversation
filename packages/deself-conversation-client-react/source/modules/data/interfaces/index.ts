d2140fe781fdfc7b39ce2db665c95be13ffdd865
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-06-18 10:42:00 +0300

    deself-conversation-client-react > dev: data/interfaces

--- packages/deself-conversation-client-react/source/modules/data/interfaces/index.ts
+++ packages/deself-conversation-client-react/source/modules/data/interfaces/index.ts
@@ -3,14 +3,39 @@
  *
  * The space can be dwelled by one or more Conversers.
  */
-
 export interface Conversation {
-    from: string; // Converser ID
-    to: string; // space ID
+    id: string;
+
+    /**
+     * UNIX timestamp
+     */
+    timestamp: number;
+
+    /**
+     * Converser ID.
+     */
+    from: string;
+
+    /**
+     * Space ID.
+     */
+    to: string;
+
+    /**
+     * Conversation message.
+     */
     data: string;
+
+    /**
+     * Parses the `data` as HTML.
+     */
+    html?: boolean;
+
+    children?: Conversation[];
 }
 
 
 export interface Converser {
     id: string;
+    identonym: string;
 }
