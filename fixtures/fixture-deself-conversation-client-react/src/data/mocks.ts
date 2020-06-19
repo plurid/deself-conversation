6c41f6230f0f72672ac3bd177c22262b185bbe96
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-06-19 08:38:15 +0300

    fixtures/fixture-deself-conversation-client-react > setup: package

--- fixtures/fixture-deself-conversation-client-react/src/data/mocks.ts
+++ fixtures/fixture-deself-conversation-client-react/src/data/mocks.ts
@@ -11,7 +11,7 @@ import {
 
 export const conversations: Conversation[] = [
     {
-        id: uuid.generate(),
+        id: 'one',
         timestamp: Date.now(),
         from: 'one',
         data: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed odio ac ipsum pharetra hendrerit. Vestibulum quis nunc vel metus condimentum maximus. Nullam gravida odio a enim convallis laoreet. Vivamus ac lacus mollis, mollis elit malesuada, lobortis enim. Sed at libero nisl. Donec in nisi ullamcorper, tempus justo et, blandit felis. Quisque dignissim urna nulla, vel malesuada diam porta vitae. Vivamus vulputate molestie leo quis varius. Fusce risus mi, auctor a erat nec, pellentesque sollicitudin turpis. Mauris eleifend sem quis lacus dapibus pellentesque. Morbi varius feugiat elementum. Donec vehicula purus sed dignissim cursus. Ut varius purus sapien, nec mattis ante consequat cursus. Suspendisse eu sollicitudin purus, ut rhoncus ex. Curabitur sed ante massa. Aenean eget ante et mauris iaculis gravida.',
